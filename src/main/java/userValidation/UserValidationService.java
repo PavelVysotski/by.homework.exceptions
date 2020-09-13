3809695e04912f5340d8bdf7dfceb9c7a3c8f07b
pvysotski <pavelve83@mail.ru>
2020-09-13 15:45:15 +0300

    Add

--- src/main/java/userValidation/UserValidationService.java
+++ src/main/java/userValidation/UserValidationService.java
@@ -5,7 +5,7 @@ public class UserValidationService {
     public static void checkUser(User user) {
         if (user.getFirstName().length() < 3 || user.getLastName().length() < 3 || user.getFirstName().length() > 15
                 || user.getLastName().length() > 15 || user.getAge() <= 0 || user.getAge() >= 120) {
-            throw new UserValidationException("Параметра данного пользователя недопустимы.");
+            throw new UserValidationException(user + "Параметры данного пользователя недопустимы.");
         } else {
             System.out.println(user + "Параметры данного пользователя в порядке.");
         }
